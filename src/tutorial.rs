89f748a756f1c059f5aa44d29e1b66c67d74e363
mg <master-g@users.noreply.github.com>
2020-09-11 20:33:58 +0800

    update

--- src/tutorial.rs
+++ src/tutorial.rs
@@ -2,6 +2,8 @@
 pub use _1_getting_started::*;
 #[cfg(feature = "chapter-2")]
 pub use _2_lighting::*;
+#[cfg(feature = "chapter-3")]
+pub use _3_model_loading::*;
 use internal::*;
 
 mod internal;
@@ -9,3 +11,5 @@ mod internal;
 pub mod _1_getting_started;
 #[cfg(feature = "chapter-2")]
 pub mod _2_lighting;
+#[cfg(feature = "chapter-3")]
+pub mod _3_model_loading;
---
+++ src/tutorial/_3_model_loading.rs
@@ -0,0 +1,3 @@
+pub mod _1_model_loading;
+
+pub use _1_model_loading::*;
